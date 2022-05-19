3a00b79b8977a4cbf50915345aa80f4facbd745e
Yulia Egorova <youlkin@mail.ru>
2022-05-19 19:56:37 +0300

    task4-done

--- Homework2/task2.py
+++ Homework2/task2.py
@@ -10,19 +10,20 @@ from random import randint, random
 
 candies = 2021
 while (candies > 0):
-
     print('Первый игрок берет конфеты, но не более 28 шт.!')
-    user_1 = int(randint(1, 28))
-    print(user_1)
+    user_1 = int(input())
+    if user_1 > 28 or user_1 <= 0:
+        print('Неверное кол-во конфет!')
+        continue
     print()
     print('Второй игрок берет конфеты, но не более 28 шт.!')
-    user_2 = int(randint(1, 28))
-    print(user_2)
+    user_2 = int(input())
+    while (user_2 > 28 or user_2 <= 0):
+        input('Неверное кол-во конфет! Берите сколько обозначено! Не более не менее: ')
     print()
     candies -= user_1 + user_2
     print()
     if candies < 0 or user_1 > candies or user_2 > candies:
-
         break
 
 print('Игра окончена! Осталось ', candies, ' конфет')
--- Homework2/task4.py
+++ Homework2/task4.py
@@ -2,19 +2,26 @@
 #    Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 
-#my_file = 'AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE'
-with open('income_file.txt', 'w') as my_file:
-    my_file.write('AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE')
-    cout = 1
-    for i in range(len(my_file)):
-        if i <= len(my_file):
-            if my_file[i] == my_file[i + 1]:
-                cout += 1
-            else:
-                a = my_file[i]
-                print(cout, my_file[i])
-                cout = 1
+# my_file = open('income_file.txt', 'w')
+# my_file.write('AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE')
 
-with open('outcome_file.txt', 'w') as my_new_file:
-    my_new_file.write(str(cout) + my_file[i])
-#print(str(cout) + my_file[i])
+
+file = open('income_file.txt', 'r')
+content = file.read()
+print(content)
+new = ''
+cout = 1
+for i in range(len(content) - 1):
+    if i <= len(content):
+        if content[i] == content[i + 1]:
+            cout += 1
+        else:
+            a = content[i]
+            new += str(cout) + content[i]
+            cout = 1
+new += str(cout) + content[i]
+print(new)
+file.close()
+new_file = open('outcome_file.txt', 'w')
+new_file.write(new)
+new_file.close()
