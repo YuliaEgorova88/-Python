bda47dc8c7fd82647cbfb65d2bbefe565d789a4e
Yulia Egorova <youlkin@mail.ru>
2022-05-19 15:10:15 +0300

    homework3 failed((((

--- Homework2/task2.py
+++ Homework2/task2.py
@@ -4,3 +4,25 @@
 # a) Добавьте игру против бота
 
 # b) Подумайте как наделить бота "интеллектом"
+
+from random import randint, random
+
+
+candies = 2021
+while (candies > 0):
+
+    print('Первый игрок берет конфеты, но не более 28 шт.!')
+    user_1 = int(randint(1, 28))
+    print(user_1)
+    print()
+    print('Второй игрок берет конфеты, но не более 28 шт.!')
+    user_2 = int(randint(1, 28))
+    print(user_2)
+    print()
+    candies -= user_1 + user_2
+    print()
+    if candies < 0 or user_1 > candies or user_2 > candies:
+
+        break
+
+print('Игра окончена! Осталось ', candies, ' конфет')
--- Homework2/task4.py
+++ Homework2/task4.py
@@ -1,2 +1,20 @@
 # 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 #    Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+
+#my_file = 'AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE'
+with open('income_file.txt', 'w') as my_file:
+    my_file.write('AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE')
+    cout = 1
+    for i in range(len(my_file)):
+        if i <= len(my_file):
+            if my_file[i] == my_file[i + 1]:
+                cout += 1
+            else:
+                a = my_file[i]
+                print(cout, my_file[i])
+                cout = 1
+
+with open('outcome_file.txt', 'w') as my_new_file:
+    my_new_file.write(str(cout) + my_file[i])
+#print(str(cout) + my_file[i])
